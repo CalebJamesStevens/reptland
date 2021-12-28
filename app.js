e8bc8839b59d432b2936c9922996748207234066
Caleb Stevens <calebjamesstevens219@gmail.com>
2021-12-27 20:31:49 -0800

    install dependencies

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//DB Config
+const db = require('./config/keys').MongoURI;
+
+//Connect to Mongo
+mongoose.connect(db, {useNewUrlParser: true})
+    .then(() => console.log('DB Connected'))
+    .catch(err => console.log(err));
 
 
 //EJS
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://caleb:gj5M9Pt8K@cluster0.ggj2f.mongodb.net/generaldb?retryWrites=true&w=majority'
+}
