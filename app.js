730e58fca4e9f005d7c3f79e2d623266f678deab
Caleb Stevens <calebjamesstevens219@gmail.com>
2021-12-27 19:12:41 -0800

    Set up server

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, console.log(`Server start on port: ${PORT}`));
