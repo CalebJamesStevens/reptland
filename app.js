8e551d2f6bb9341552316748814488ed81511a12
Caleb Stevens <calebjamesstevens219@gmail.com>
2022-01-20 22:39:36 -0800

    Add env file

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const session = require('express-session');
+
 const passport = require('passport');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
@@ -10,11 +11,20 @@ const Post = require('./models/Post');
 const {ObjectId} = require('mongodb');
 const app = express();
 
+//ENV
+require('dotenv').config()
+
+//AWS and Multer stuff
+const multer = require('multer')
+const Aws = require('aws-sdk')
+
+//Authentication
 require('./config/passport')(passport);
 
 //DB Config
-const db = require('./config/keys').MongoURI;
+const db = process.env.MONGO_URI;
 const User = require('./models/User');
+
 
 //Connect to Mongo
 mongoose.connect(db, {useNewUrlParser: true})
@@ -48,7 +58,7 @@ app.use(methodOverride('_method'))
 // Give access to currentUser without having to pass in 
 app.use(function(req, res, next) {
     res.locals.currentUser = req.user;
-
+    console.log(`ASDFddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd ${process.env.MONGO_URI}`)
     next();
 });
 
--- config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    MongoURI: 'mongodb+srv://caleb:gj5M9Pt8K@cluster0.ggj2f.mongodb.net/generaldb?retryWrites=true&w=majority'
-}
